99d81e36571addc488e545e0639af3374100321f
Divya Gandhi <divyagandhi274@gmail.com>
2024-11-21 15:21:52 +0530

    Initial commit

---
+++ Leaky Bucket/Client.py
@@ -0,0 +1,22 @@
+import socket
+
+
+HOST = '192.168.29.225' 
+PORT = 5001
+
+# Create a TCP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect((HOST, PORT))
+
+
+
+while True:
+    message=input("Enter data:")
+    client_socket.sendall(message.encode())
+    if(message=="end"):
+        break
+print("End")
+# Close the client socket
+client_socket.close()
---
+++ Leaky Bucket/Server.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+import time
+
+MAX_SIZE = 5 
+LEAK_RATE = 0.5
+HOST = '0.0.0.0'
+PORT = 5001
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+
+
+server_socket.listen()
+print(f"Server is listening on {HOST}:{PORT}...")
+
+# Accept a client connection
+conn, addr = server_socket.accept()
+print(f"Connected by {addr}")
+
+received_data = []
+
+
+def leaky():
+    while True:
+        time.sleep(1 / LEAK_RATE)  
+        if received_data:
+            leaked_data = received_data.pop(0)  
+            print(f"Leaked data: {leaked_data}")
+        else:
+            print("Bucket is empty, no data to leak.")
+
+
+def addInQueue():
+    with conn:
+        while True:
+           
+            data = conn.recv(1024)
+            if not data or data.decode() == "end":
+                print("Client has ended communication")
+                break
+
+           
+            if len(received_data) < MAX_SIZE:
+                received_data.append(data.decode())
+                print(f"Received data: {received_data}")
+            else:
+                print("Queue is full. Discarding new data.")
+
+# Run the leaky bucket function in a separate thread
+leaky_thread = threading.Thread(target=leaky, daemon=True)
+leaky_thread.start()
+
+
+addInQueue()
+
+# Close the server socket
+server_socket.close()
