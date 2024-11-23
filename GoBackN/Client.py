a7effa42d1f549ea0b459dfb48210cb05ca92fc9
Divya Gandhi <divyagandhi274@gmail.com>
2024-11-23 17:20:44 +0530

    modified GBN

--- GoBackN/Client.py
+++ GoBackN/Client.py
@@ -4,34 +4,34 @@ import time
 HOST = '192.168.29.225'
 PORT = 5000
 
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((HOST,PORT))
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+TICK_SPEED=0.2
 
-s.connect((HOST, PORT))
-TICK_SPEED = .2
+seq_nums=int(input("Enter number of sequences:"))
+window_size=int(input("Enter window size:"))
 
-seq_nums = int(input("Enter the number of sequence numbers: "))
-window_size = int(input("Enter the window size: "))
-time.sleep(1)
 s.send(str(seq_nums).encode())
-data = [i for i in range(seq_nums)]
-ack = 0
-while ack < seq_nums:
-	end = min(ack + window_size, seq_nums)
-	window = data[ack:end]
-	for i in range(end - ack):
-		print(f"Sending packet {window[i]}")
-		s.send(str(window[i]).encode())
-		time.sleep(TICK_SPEED)
-	ack = s.recv(1024).decode()
-	ack = int(ack.split(',')[-1])
-	print(f"Received ack {ack}")
-	for i in range(ack):
-		if window:
-			window.pop(0)
-		else:
-			break
-print("All packets sent")
-print("Closing connection")
-s.send(b'exit')
+
+
+ack=0
+data=[i for i in range(seq_nums)]
+while ack<seq_nums:
+    end=min(ack+window_size,seq_nums)
+    window = data[ack:end]
+    for i in range(end-ack):
+        print(f"Send data: {window[i]}")
+        s.send(str(window[i]).encode())
+        time.sleep(TICK_SPEED)
+    ack=s.recv(1024).decode()
+    ack=int(ack.split(",")[-1])
+    print(f"Recieved {ack}")
+    for i in range (ack):
+        if window:
+            window.pop(0)
+        else:
+            break
+print("All packets transferred")
+print("Connection ended")
 s.close()
--- GoBackN/Server.py
+++ GoBackN/Server.py
@@ -1,39 +1,36 @@
-import socket
+import socket 
 import time
 import random
 
-HOST = '0.0.0.0'
-PORT =5000
+HOST='0.0.0.0'
+PORT=5000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.bind((HOST, PORT))
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((HOST,PORT))
 s.listen(1)
 
-conn, addr = s.accept()
-print('Connected by', addr)
-TICK_SPEED =0.2
-time.sleep(1)
-seq_nums = int(conn.recv(1024).decode())
-# window_size = 1
+conn,addr=s.accept()
+TICK_SPEED=0.2
 
-ack = 0
-curr = 0
-while ack < seq_nums:
-	received = conn.recv(1024).decode()
-	if received == 'exit':
-		break
-	received = int(received)
-	print(f"Received packet {received}")
-	if random.choice([True, False]) and received == curr:
-		curr += 1
-		ack = curr
-		print(f'Sending ack {ack}')
-		conn.sendall((',' + str(ack)).encode())
-	else:
-		print(f"Packet lost {received}, Expected {curr}")
-		conn.sendall((',' + str(curr)).encode())
-	time.sleep(TICK_SPEED)
-print("All packets received")
-print("Closing connection")
+seq_nums=conn.recv(1024).decode()
+seq_nums=int(seq_nums)
+ack=0
+curr=0
+
+while ack<seq_nums:
+    received=conn.recv(1024).decode()
+    if received=='exit':
+        break
+    received=int(received)
+    print(f"Received:{received}")
+    if random.choice([True,False]) and curr==ack:
+        curr+=1
+        ack=curr
+        print(f"Sending ack:{received}")
+        conn.sendall((',' + str(ack)).encode())
+    else:
+        print("Paket failed to send")
+    time.sleep(TICK_SPEED)
+print("Packets received")
+print("Connection ended")
 conn.close()
