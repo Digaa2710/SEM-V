eb5023f8ee8a4ad11422aed54c7bfcbdf8574572
Divya Gandhi <divyagandhi274@gmail.com>
2024-11-23 20:49:48 +0530

    Correct byte stuffing

--- Framing/ByteStuffing.py
+++ Framing/ByteStuffing.py
@@ -3,12 +3,12 @@ tail=list(input("Enter data tail:"))
 data=list(input("Enter data:"))
 
 i=0
-j=0
+
 while i<len(data):
-    if(i<len(header) or tail[j]==data[i]):
+    if( header[0]==data[i] or tail[0]==data[i]):
         data.insert(i,'[$]')
         i+=1
     i+=1
-    j+=1
+    
 final_data=header+data+tail
 print(f"Final result:{final_data}")
