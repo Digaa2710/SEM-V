d0d27782d1722e90e5c60f1a434d56cc4b3ef52c
Divya Gandhi <divyagandhi274@gmail.com>
2024-11-23 22:31:04 +0530

    Added inputs in character count

--- Framing/CharacterCount.py
+++ Framing/CharacterCount.py
@@ -1,4 +1,9 @@
-frame=list(input("Enter frames:"))
+frame=[]
+n=int(input("Enter number of frames:"))
+for i in range(n):
+    frames=int(input("Enter frame value:"))
+    frame.append(frames)
+print(frame)
 count=0
 for i in range(len(frame)):
     count=count+1
